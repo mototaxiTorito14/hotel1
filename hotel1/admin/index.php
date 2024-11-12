1b83f59805b46243e516106fdf459f5790ba40ba
Luis Ernesto Gomez Ramirez <99620094+LuisGo1@users.noreply.github.com>
2024-11-11 22:29:59 -0600

    cambios todo

--- hotel1/admin/index.php
+++ hotel1/admin/index.php
@@ -1,11 +1,129 @@
 <?php
 include "../admin/includes/header.php";
 ?>
-    <!-- Contenido Principal -->
-    <div class="main-content">
-        <h1>Bienvenido al Panel de Administración</h1>
-        <p>Seleccione una opción del menú lateral para gestionar las tablas del sistema.</p>
+<div></div>
+    <!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Niveles</title>
+    <link rel="stylesheet" href="../css/styles1.css">
+</head>
+<body>
+    <div class="container">
+        
+        
+        <section class="levels">
+            <div class="level-card">Nivel 1</div>
+            <div class="level-card">Nivel 2</div>
+            <div class="level-card">Nivel 3</div>
+            <div class="level-card">Nivel 4</div>
+        </section>
+        
+        <section class="rooms">
+           
+            <div class="room-card">
+                <h3>Doble</h3>
+                <p>Cama + TV + Baño</p>
+                <p>150</p>
+                <p class="price">$ 75.00</p>
+            </div>
+            <div class="room-card">
+                <h3>Simple</h3>
+                <p>Cama + TV + Baño</p>
+                <p>182</p>
+                <p class="price">$ 50.00</p>
+            </div>
+            <div class="room-card">
+                <h3>Simple</h3>
+                <p>Cama + TV + Baño</p>
+                <p>124</p>
+                <p class="price">$ 50.00</p>
+            </div>
+            <div class="room-card">
+                <h3>Simple</h3>
+                <p>Cama + TV + Baño</p>
+                <p>122</p>
+                <p class="price">$ 50.00</p>
+            </div>
+            <div class="room-card">
+                <h3>Simple</h3>
+                <p>Cama + TV + Baño</p>
+                <p>155</p>
+                <p class="price">$ 50.00</p>
+            </div>
+            <div class="room-card">
+                <h3>Simple</h3>
+                <p>Cama + TV + Baño</p>
+                <p>133</p>
+                <p class="price">$ 40.00</p>
+            </div>
+            
+            
+            <!-- Agregar más tarjetas de habitación aquí -->
+        </section>
+        <section class="rooms">
+            
+            
+            <div class="room-card">
+                <h3>Doble</h3>
+                <p>Cama + TV + Baño</p>
+                <p>103</p>
+                <p class="price">$ 75.00</p>
+            </div>
+            <div class="room-card">
+                <h3>Doble</h3>
+                <p>Cama + TV + Baño</p>
+                <p>104</p>
+                <p class="price">$ 55.00</p>
+            </div>
+            <div class="room-card">
+                <h3>Doble</h3>
+                <p>Cama + TV + Baño</p>
+                <p>131</p>
+                <p class="price">$ 35.00</p>
+            </div>
+            
+            <div class="room-card">
+                <h3>Simple</h3>
+                <p>Cama + TV + Baño</p>
+                <p>142</p>
+                <p class="price">$ 40.00</p>
+            </div>
+            <div class="room-card">
+                <h3>Simple</h3>
+                <p>Cama + TV + Baño</p>
+                <p>134</p>
+                <p class="price">$ 50.00</p>
+            </div>
+            <div class="room-card">
+                <h3>Simple</h3>
+                <p>Cama + TV + Baño</p>
+                <p>100</p>
+                <p class="price">$ 30.00</p>
+            </div>
+          
+            <!-- Agregar más tarjetas de habitación aquí -->
+        </section>
+        
+        <section class="info">
+            
+            <div class="categories-card">
+                <h3>Habitaciones Disponibles</h3>
+                <h1> 1</h1>
+            </div>
+            <div class="categories-card">
+                <h3>Habitaciones Ocupadas</h3>
+                <h1> 5</h1>
+                
+            </div>
+        </section>
     </div>
+
+</body>
+</html>
+
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.min.js"></script>
